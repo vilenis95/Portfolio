e8cdd5ac39fee33efc4a51040d19f9bbc6195304
Ruslan <134260052+russell3002@users.noreply.github.com>
2023-10-05 23:48:52 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    const initialFileButtonText = $(".input__file-button-text").text();
+    let initialFileButtonText = $(".input__file-button-text").text();
 
     $('.input__file').each(function() {
-        const input = $(this);
-        const label = input.next();
-        const labelVal = label.find('.input__file-button-text').text();
+        let input = $(this);
+        let label = input.next();
+        let labelVal = label.find('.input__file-button-text').text();
 
         input.change(function(e) {
             let countFiles = '';
@@ -12,35 +12,80 @@ $(document).ready(function() {
                 countFiles = this.files.length;
 
             if (countFiles)
-                label.find('.input__file-button-text').text('Выбрано файлов: ' + countFiles);
+                label.find('.input__file-button-text').text('Files selected: ' + countFiles);
             else
                 label.find('.input__file-button-text').text(labelVal);
         });
     });
 
-    const portfolioCards = $("#portfolioCards");
-    const addBlockButton = $("#addBlockButton");
-    const addBlockForm = $("#addBlockForm");
-    const submitBlockButton = $("#submitBlockButton");
+    let portfolioCards = $("#portfolioCards");
+    let addBlockButton = $("#addBlockButton");
+    let addBlockForm = $("#addBlockForm");
+    let submitBlockButton = $("#submitBlockButton");
 
-    addBlockButton.click(function() {
-        portfolioCards.fadeOut("slow");
-        addBlockForm.fadeIn("slow");
+    portfolioCards.on("click", ".card a", function(event) {
+        event.preventDefault(); 
+
+        $(".card").removeClass("opened");
+        $(this).closest(".card").addClass("opened");
+
+        $(".card").hide();
+        $(".card.opened").show();
+
+        $(this).siblings(".block-content").slideDown("slow");
+        $(this).siblings(".close-block-button").show();
+
+        addBlockButton.hide();
+    });
+
+    portfolioCards.on("click", ".close-block-button", function(event) {
+        event.preventDefault(); 
+
+        $(this).closest(".block-content").slideUp("slow", function () {
+            $(".card").show();
+            addBlockButton.show();
+        });
+        $(this).hide();
+        $(".close-block-button").css("display", "block")
+
+        $(this).closest(".card").removeClass("opened");
+
+        
+    });
+
+    portfolioCards.on("click", "#addBlockButton", function() {
+        portfolioCards.fadeOut("slow", function() {
+            addBlockForm.fadeIn("slow");
+        });
+    });
+
+    addBlockForm.on("click", ".close-form-button", function() {
+        addBlockForm.fadeOut("slow", function() {
+            portfolioCards.fadeIn("slow");
+        });
     });
 
     submitBlockButton.click(function() {
-        const blockTitle = $("#blockTitle").val();
-        const blockText = $("#blockText").val();
-        const blockImage = $("#blockImage")[0].files[0];
-        const blockCategory = $("#blockCategory").val();
+        let blockTitle = $("#blockTitle").val();
+        let blockText = $("#blockText").val();
+        let blockImage = $("#blockImage")[0].files[0];
 
         if (blockImage) {
-            const block = $("<div>").addClass("card");
-            const image = $("<img>").attr("src", URL.createObjectURL(blockImage));
-            const category = $("<p>").text(blockCategory);
-            const title = $("<a>").text(blockTitle);
+            let block = $("<div>").addClass("card");
+            let image = $("<img>").attr("src", URL.createObjectURL(blockImage));
+            let category = $("<p>").text($("#blockCategory").val());
+            let title = $("<a>").text(blockTitle);
 
-            block.append(image, category, title);
+            let backBtn = $("<button>").addClass("close-block-button");
+            let backImg = $("<img>").attr("src", "img/back.svg").attr("alt", "");
+            backBtn.append(backImg);
+
+            let hiddenText = $("<p>").addClass("hidden-text").text(blockText);
+
+            let blockContent = $("<div>").addClass("block-content").css("display", "none");
+            blockContent.append(backBtn, hiddenText);
+
+            block.append(image, category, title, blockContent);
             portfolioCards.prepend(block);
 
             $(".input__file-button-text").text(initialFileButtonText);
@@ -50,11 +95,11 @@ $(document).ready(function() {
             $("#blockImage").val("");
             $("#blockCategory").val("All");
 
-            addBlockForm.fadeOut("slow");
-            portfolioCards.fadeIn("slow");
+            addBlockForm.fadeOut("slow", function() {
+                portfolioCards.fadeIn("slow");
+            });
         } else {
             alert("Choose an image file.");
         }
     });
 });
-
